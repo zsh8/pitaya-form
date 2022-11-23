50ec0af67bdadb0ba55d7d2923ef91207e97a799
Zahra Shamsi <shamsi@soc1.ir>
2022-11-23 17:53:33 +0330

    Update all dependencies and add minifier and peerDependencies

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,12 +1,13 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
+    "@storybook/addon-interactions",
+    "@storybook/preset-scss",
   ],
-  "framework": "@storybook/react"
-}
+  framework: "@storybook/react",
+  core: {
+    builder: "webpack5",
+  },
+};
